7b21bc0ee5cfc2e7e6d2093adb5ca9d5469de388
usman_rizwan <usmanrizwan771@gmail.com>
2024-03-07 22:35:30 +0500

    Added login and jwt

--- routes/user.js
+++ routes/user.js
@@ -1,24 +1,65 @@
 import express from "express";
 import User from "../models/UserModel.js";
 import bodyParser from "body-parser";
-
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
 const router = express.Router();
 
-router.use(bodyParser.json()); //middleware to parse request body to json
+router.use(bodyParser.json()); // Middleware to parse request body to JSON
 
 router.post("/", async (req, res) => {
-  // console.log('req.body--->',req.body);
   try {
-    const user = new User(req.body);
+    const existingUser = await User.findOne({ email: req.body.email });
+
+    if (existingUser) {
+      return res.status(400).send({
+        status: 400,
+        message: "User with the same email already exists",
+      });
+    }
+    const saltRounds = 10;
+    const password = await bcrypt.hash(req.body.password, saltRounds);
+    const user = new User({ ...req.body, password });
+    
     const newUser = await user.save();
-    //     const data = req.body
-    // const user = User.create(data)
-    // user.save();
-    return res
-      .status(200)
-      .send({ status: 200, message: "User Added in db ", user: newUser });
+    const token = jwt.sign({_id:newUser._id},"practicehaisab")
+
+    return res.status(200).send({
+      status: 200,
+      message: "User added to the database",
+      user: newUser,
+      token
+    });
   } catch (error) {
-    res.status(400).status({ status: 400, message: error.message });
+    console.error("Error:", error);
+    return res
+      .status(500)
+      .send({ status: 500, message: "Internal Server Error" });
+  }
+});
+router.post("/login", async (req, res) => {
+  try {
+    const {email , password } = req.body
+    // const user = await User.findOne({ email }).then(res => res.toObject());
+    const findUser = await User.findOne({ email });
+    const user =  findUser.toObject();
+
+    if (!user) {
+      return res.status(401).send({message:"User not found"})
+    }
+    const match = await bcrypt.compare(password, user.password);
+    if (!match) {
+      return res.status(403).send({message:"Incorrect password"})
+    }
+    delete  user.password;
+    const token = jwt.sign({_id:user._id},"practicehaisab")
+      return res.status(200).send({ message: "User found successfully" , user , token });
+   
+  } catch (error) {
+    console.error("Error:", error);
+    return res
+      .status(500)
+      .send({ status: 500, message: error.message });
   }
 });
 
