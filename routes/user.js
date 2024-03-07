766455265c7b19d4c14b00cd418fef702173221b
AbdulKarim018 <lackhorse.ak@gmail.com>
2024-03-07 20:14:42 +0500

    fix users route
    added body-parser to parse request body to json

--- routes/user.js
+++ routes/user.js
@@ -1,34 +1,40 @@
 import express from "express";
 import User from "../models/UserModel.js";
+import bodyParser from "body-parser";
+
 const router = express.Router();
 
+router.use(bodyParser.json()); //middleware to parse request body to json
 
-router.post("/", async(req, res) => {
+router.post("/", async (req, res) => {
   // console.log('req.body--->',req.body);
   try {
     const user = new User(req.body);
     const newUser = await user.save();
-//     const data = req.body
-// const user = User.create(data)
-// user.save();
-   return res.status(200).send({status:200, message: "User Added in db " ,user:newUser})
+    //     const data = req.body
+    // const user = User.create(data)
+    // user.save();
+    return res
+      .status(200)
+      .send({ status: 200, message: "User Added in db ", user: newUser });
   } catch (error) {
-    res.status(400).status({status:400,message: error.message });
+    res.status(400).status({ status: 400, message: error.message });
   }
 });
-    // const users = [
-    //   {
-    //     id: 1,
-    //     name: "Usman",
-    //     email: "usman@gmail.com",
-    //   },
-    //   {
-    //     id: 2,
-    //     name: "Ali",
-    //     email: "ali@gmail.com",
-    //   },
-    // ];
-    // router.get("/", (req, res) => {
-    //   res.status(200).send({ users });
-    // });
+
+// const users = [
+//   {
+//     id: 1,
+//     name: "Usman",
+//     email: "usman@gmail.com",
+//   },
+//   {
+//     id: 2,
+//     name: "Ali",
+//     email: "ali@gmail.com",
+//   },
+// ];
+// router.get("/", (req, res) => {
+//   res.status(200).send({ users });
+// });
 export default router;
